21c02776d51e9a4af9e4dbba927e225390286a78
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2024-08-12 21:47:22 +0200

    new file:   app/(tabs)/database.tsx modified:   app/_layout.tsx modified:   package-lock.json modified:   package.json

---
+++ app/(tabs)/database.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import * as SQLite from 'expo-sqlite';
+
+
+const database = () => {
+  return (
+    <View>
+      <Text>database</Text>
+    </View>
+  )
+}
+
+export default database
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,15 @@
 import { Stack } from "expo-router"
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
+import * as SQLite from "expo-sqlite";
+const db = SQLite.openDatabaseSync("AudChor-DB");
+
+
 
 const StackLayout = () => {
+  useDrizzleStudio(db);
+
   return (
+    
     <Stack>
         <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
     </Stack>
