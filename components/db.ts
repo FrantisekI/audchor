fd3f29fde322df498fb6f0f586b3d6bae9ac0beb
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2024-08-14 11:11:23 +0200

    modified:   components/db.ts modified:   package-lock.json modified:   package.json

--- components/db.ts
+++ components/db.ts
@@ -2,27 +2,35 @@ import * as SQLite from 'expo-sqlite';
 
 const db = await SQLite.openDatabaseAsync('MainDB');
 const initDatabase = async () => {
-await db.execAsync(`
-    PRAGMA journal_mode = WAL;
-    CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY NOT NULL, 
-        name TEXT NOT NULL, 
-        pathto TEXT NOT NULL);
+    try {
+        await db.execAsync(`
+        PRAGMA journal_mode = WAL;
+        CREATE TABLE IF NOT EXISTS songs (
+            id INTEGER PRIMARY KEY NOT NULL, 
+            name TEXT NOT NULL, 
+            pathto TEXT NOT NULL);
+    
+        CREATE TABLE IF NOT EXISTS tags (
+            id INTEGER PRIMARY KEY NOT NULL,
+            name TEXT NOT NULL,
+            color TEXT NOT NULL);
+    
+        CREATE TABLE IF NOT EXISTS attributes (
+            id INTEGER PRIMARY KEY NOT NULL,
+            name TEXT NOT NOT NULL,
+            action TEXT NOT NULL,
+            priority INTEGER NOT NULL);
+    
+        CREATE TABLE IF NOT EXISTS song_tags (
+            PRIMARY KEY (song_id, tag_id),
+            song_id INTEGER NOT NULL,
+            tag_id INTEGER NOT NULL);
+        `);
+        
+        console.log('Database initialized');
+    } catch (e) {
+        console.log(e);
+    };
+};
 
-    CREATE TABLE IF NOT EXISTS tags (
-        id INTEGER PRIMARY KEY NOT NULL,
-        name TEXT NOT NULL,
-        color TEXT NOT NULL);
-
-    CREATE TABLE IF NOT EXISTS attributes (
-        id INTEGER PRIMARY KEY NOT NULL,
-        name TEXT NOT NULL,
-        action TEXT NOT NULL,
-        priority INTEGER NOT NULL);
-
-    CREATE TABLE IF NOT EXISTS song_tags (
-        PRIMARY KEY (song_id, tag_id),
-        song_id INTEGER NOT NULL,
-        tag_id INTEGER NOT NULL);
-    `);
-}
+export { initDatabase }
